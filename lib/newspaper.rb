4fa121349ed101b5f48cc45492e8f18e95d05c47
sts10 <sschlinkert@gmail.com>
2014-11-19 22:42:06 -0500

    prints but doesn't preserve html yet

---
+++ lib/newspaper.rb
@@ -0,0 +1,45 @@
+require 'nokogiri'
+require 'open-uri' # a module not a gem
+require 'erb'
+require 'date'
+require 'tzinfo'
+require 'pry'
+
+require_relative './post.rb'
+require_relative './post_scraper.rb'
+require_relative '../following.rb'
+
+class Newspaper
+  def print
+    @posts_array = []
+
+    $following.each do |followed_url|
+      this_blog = PostScraper.new(followed_url)
+      @posts_array = @posts_array + this_blog.scrape_posts
+
+      
+    end
+    @posts_array.flatten
+    puts "Printing your newspaper..."
+
+    @posts_array.each do |post| 
+      puts post.file_name 
+      # binding.pry
+    end
+
+    template_doc= File.open('../templates/newspaper.html.erb', "r")
+
+    template = ERB.new(template_doc.read)
+    
+    File.open('../my_newspaper.html', "w") do |f|
+        f.write(
+          template.result(binding) # result is an ERB method. `binding` here means we're passing all local variables to the template. 
+        )
+      f.close
+    end
+  end
+
+end
+
+my_newspaper = Newspaper.new
+my_newspaper.print
--- lib/post_scraper.rb
+++ lib/post_scraper.rb
@@ -10,22 +10,25 @@ class PostScraper
 
   def scrape_posts
     posts = @doc.css('.blog_post')
+    post_objects =[]
 
     posts.each do |post|
       this_post = Post.new
       this_post.content = post.text
-      this_post.file_name = post.attribute('id')
+      this_post.file_name = post.attribute('id').value
       this_post.user_name = @url.to_s.partition('~').last.gsub('/','')
 
       puts this_post.file_name
-  
+      post_objects << this_post
       # puts this_post.user_name
     end
+
+    post_objects
   end
 
 end 
 
 
-my_blog = PostScraper.new('http://totallynuclear.club/~schlink/')
+# my_blog = PostScraper.new('http://totallynuclear.club/~schlink/')
 
-my_blog.scrape_posts
+# my_blog.scrape_posts
