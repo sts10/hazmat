6b0bd53b600665ed1a71442bbbc98e877e1f5cbf
sts10 <sschlinkert@gmail.com>
2014-11-22 01:33:29 -0500

    reorganized css and js files, now j and k navigate between posts, and posts have max height of 55px with overflow set to auto

---
+++ js/app.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+
+  var post_index = -1;
+  var addressValue = '';
+
+  // listens for any navigation keypress activity
+  $(document).keydown(function(e){
+
+    // if all text boxes are out of focus...
+    if ($("input").is(':focus') == false && $("textarea").is(':focus') == false) {
+
+      switch(e.which) {
+
+        // code look-up: http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+
+        case 38: // up
+        case 75: // k
+        case 80: // p
+          Navigate(-1);
+        break;
+
+        case 40: // down
+        case 74: // j
+        case 78: // n
+          console.log('down')
+          Navigate(1);
+        break;
+
+        // case 13: // enter/return/carriage return
+        //   window.location.href = addressValue;
+        // break; 
+
+        default: return; // exit this handler for other keys
+      }
+      e.preventDefault(); // prevent the default action (scroll / move caret)
+    }
+  });
+
+
+  var Navigate = function(diff) {
+      post_index += diff;
+      var post_list = $(".blog_post");
+      if (post_index >= post_list.length){
+           post_index = 0;
+      }
+      if (post_index < 0){
+           post_index = post_list.length - 1;
+      }
+
+      // apply css class to selected post, and remove it from all other posts
+      var cssClass = "selected_post";
+      post_list.removeClass(cssClass).eq(post_index).addClass(cssClass);
+
+      // scroll body to the selected post
+      $('html, body').animate({
+          scrollTop: $(".selected_post").offset().top - 100
+      }, 20);
+
+      // set global variable addressVariable to the address of the selected post
+      // addressValue = $(".selected_post").attr("href");
+  };
+
+
+  // on window resize
+  // $( window ).resize(function() {
+    // console.log("window bheing resized");
+    // var window_height = $( window ).height();
+    // console.log('window height is now' + window_height);
+    // $('.blog_post').attr('max-height', window_height - 80 + 'px');
+  // });
+
+});
