ec84a7ef2d8d2786f51556b47690c2bab9c37fae
sts10 <sschlinkert@gmail.com>
2014-11-23 23:29:33 -0500

    added new radiation blog to following.rb

--- following.rb
+++ following.rb
@@ -4,8 +4,9 @@ $following = [
   'http://totallynuclear.club/~afantasy',
   'http://totallynuclear.club/~schlink',
   'http://totallynuclear.club/~erik/',
+  'http://totallynuclear.club/~gunnar/',
   'http://totallynuclear.club/~ianremsen/blog.html',
   'http://totallynuclear.club/~papa/blog.html',
-  'http://google.com'
+  'http://google.com' # this is here for testing purposes obviously
 
 ]
