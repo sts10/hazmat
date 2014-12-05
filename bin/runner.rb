9719edfc8ba6b93df358fadfb40fdaba3a7ad61a
sts10 <sschlinkert@gmail.com>
2014-12-05 16:26:56 -0500

    puts newspaper.html in to public_html/hazmat/

--- bin/runner.rb
+++ bin/runner.rb
@@ -7,6 +7,9 @@ my_newspaper.print
 puts "what is your totally nuclear username?"
 username = "schlink"
 
-system "scp #{username}@totallynuclear.club:hazmat/my_newspaper.html ~/Desktop"
+system "mkdir ~/public_html/hazmat"
+system "cp css/styles.css ~/public_html/hazmat/styles.css"
+system "cp js/app.js ~/public_html/hazmat/app.js"
+system "mv my_newspaper.html ~/public_html/hazmat/my_hazmat_newspaper.html"
 
 # system "lynx my_newspaper.html"
