38de8e3d7848e761041a8398b3c1f1804c56e605
sts10 <sschlinkert@gmail.com>
2014-12-05 16:39:49 -0500

    explain where to find newspaper, and offer lynx chocie. also fixes typo in template

--- bin/runner.rb
+++ bin/runner.rb
@@ -4,12 +4,17 @@ require_relative '../config/environment'
 my_newspaper = Newspaper.new
 my_newspaper.print
 
-puts "what is your totally nuclear username?"
-username = "schlink"
-
 system "mkdir ~/public_html/hazmat"
 system "cp css/styles.css ~/public_html/hazmat/styles.css"
 system "cp js/app.js ~/public_html/hazmat/app.js"
 system "mv my_newspaper.html ~/public_html/hazmat/my_hazmat_newspaper.html"
 
-# system "lynx my_newspaper.html"
+puts "Your Hazmat newspaper is available at http://totallynuclear.club/~your_username/hazmat/my_hazmat_newspaper.html"
+puts ""
+
+puts "Alternatively, you may view your Hazmat newspaper through a console browser called Lynx."
+puts "Would you like to open your newspaper here in the console using Lynx? [y/N]"
+l_choice = gets.chomp
+if l_choice.downcase.strip == 'y'
+  system "lynx my_newspaper.html"
+end
